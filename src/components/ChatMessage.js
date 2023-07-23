b159674ba19b64ed22d3129f62ce5c4e2fc1548c
Abhishek <abhisheksinghasr734@gmail.com>
2023-07-23 20:12:36 +0530

    api key

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatMessage = ({ name, message, imgSrc }) => {
   return (
-    <div className="flex  rounded-md p-2 m-2">
+    <div className="flex bg-gray-500 rounded-md p-2 m-2">
       <img className="h-8 rounded-full" src={imgSrc} />
       <span className="font-bold mx-1 text-lg">{name}: </span>
       <span className="text-lg">{message}</span>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAuHrzl10GQo3X2jXJ2jv90nC3XfLT1MmA";
+const GOOGLE_API_KEY = "AIzaSyC9hxDWeSKMjV2aRdDU3vPk7T5zQU7t2cw";
 
 export const YOUTUBE_VIDEOS_API =
   "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
